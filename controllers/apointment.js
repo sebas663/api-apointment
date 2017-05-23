f75e3d61645b9f8f23a17ef4baaf3de3b0c504b2
sebas663 <sebastian_escuderoh@hotmail.com>
2017-05-23 20:02:35 -0300

    add promises

--- controllers/apointment.js
+++ controllers/apointment.js
@@ -1,24 +1,36 @@
 //File: controllers/apointment.js
 var mongoose    =   require('mongoose');
+
+// set Promise provider to bluebird
+mongoose.Promise = require('bluebird');
+
 var Apointment  =   require('../models/Apointment');
 
 //GET - Return all apointments in the DB
 exports.findAll = function(req, res) {
-	Apointment.find(function(err, apointments) {
-    if(err) res.send(500, err.message);
-
-    console.log('GET /apointments')
-		res.status(200).jsonp(apointments);
+	var promise = Apointment.find().exec();
+	promise.then(function(apointments) {
+		console.log('GET /apointments')
+		return res.status(200).jsonp(apointments);
+	})
+	.catch(function(err){
+		// just need one of these
+		console.log('error:', err);
+		return res.send(500, err.message);
 	});
 };
 
 //GET - Return a apointment with specified ID
 exports.findById = function(req, res) {
-	Apointment.findById(req.params.id, function(err, apointment) {
-    if(err) return res.send(500, err.message);
-
-    console.log('GET /apointments/' + req.params.id);
-		res.status(200).jsonp(apointment);
+	var promise = Apointment.findById(req.params.id).exec();
+	promise.then(function(apointment) {
+		console.log('GET /apointments/' + req.params.id);
+		return res.status(200).jsonp(apointment);
+	})
+	.catch(function(err){
+		// just need one of these
+		console.log('error:', err);
+		return res.send(500, err.message);
 	});
 };
 
@@ -36,34 +48,53 @@ exports.add = function(req, res) {
         status_code: req.body.status_code
 	});
 
-	apointment.save(function(err, apointment) {
-		if(err) return res.send(500, err.message);
-    res.status(200).jsonp(apointment);
+	var promise = apointment.save();
+
+	promise.then(function(apointment) {
+		return res.status(200).jsonp(apointment);
+	})
+	.catch(function(err){
+		// just need one of these
+		console.log('error:', err);
+		return res.send(500, err.message);
 	});
 };
 
 //PUT - Update a register already exists
 exports.update= function(req, res) {
-	Apointment.findById(req.params.id, function(err, apointment) {
+
+	var promise = Apointment.findById(req.params.id).exec();
+
+	promise.then(function(apointment) {
 		apointment.id = req.body.id,
 		apointment.id_patient = req.body.id_patient,
         apointment.id_turn_diary = req.body.id_turn_diary,
         apointment.coment = req.body.coment,
         apointment.overturned = req.body.overturned,
         apointment.status_code = req.body.status_code
-		apointment.save(function(err) {
-			if(err) return res.send(500, err.message);
-      res.status(200).jsonp(apointment);
-		});
+	})
+	.then(function(apointment) {
+		return res.status(200).jsonp(apointment);
+	})
+	.catch(function(err){
+		// just need one of these
+		console.log('error:', err);
+		return res.send(500, err.message);
 	});
 };
 
 //DELETE - Delete a Apointment with specified ID
 exports.delete = function(req, res) {
-	Apointment.findById(req.params.id, function(err, apointment) {
-		apointment.remove(function(err) {
-			if(err) return res.send(500, err.message);
-      res.status(200);
-		})
+	var promise = Apointment.findById(req.params.id).exec();
+	promise.then(function(apointment) {
+		return apointment.remove(); // returns a promise
+	})
+	.then(function(apointment) {
+		return res.status(200);
+	})
+	.catch(function(err){
+		// just need one of these
+		console.log('error:', err);
+		return res.send(500, err.message);
 	});
 };
